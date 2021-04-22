3336763098bef41f721145959077495f34213537
kodarit11 <taavi.makela@gmail.com>
2021-04-22 19:32:44 +0300

    liikkumis scripti aloitettu

---
+++ 2021_kevat_to1830_unity3d_Git/Assets/Scripts/HahmoOhjain.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HahmoOhjain : MonoBehaviour
+{
+    public float juoksuNopeus = 3.0f;
+    public float hiireNopeus = 3.0f;
+
+    private float vertikaalinenPyorinta = 0;
+    private float horisontaalinenPyorinta = 0;
+    private Vector3 liikesuunta = Vector3.zero;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
